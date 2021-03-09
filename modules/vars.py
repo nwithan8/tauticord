e2fbc4fe0c452ba63fa946feef5fc7aad17fe1e3
Nate Harris <nwithan8@users.noreply.github.com>
2021-03-08 20:43:50 -0700

    Bug fix

--- modules/vars.py
+++ modules/vars.py
@@ -12,7 +12,7 @@ switcher = {
 media_type_icons = {'episode': '📺', 'track': '🎧', 'movie': '🎞', 'clip': '🎬', 'photo': '🖼', 'live': '📡'}
 
 sessions_message = """{stream_count} {word}"""
-transcodes_message = """{transcode_count} {word}}"""
+transcodes_message = """{transcode_count} {word}"""
 bandwidth_message = """🌐 {bandwidth}"""
 lan_bandwidth_message = """(🏠 {bandwidth})"""
 
