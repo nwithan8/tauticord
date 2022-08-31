66f7502916e7b3c13c0df926750e18c764bab5ee
Nate Harris <nharris@easypost.com>
2022-08-30 19:41:53 -0600

    - Allow config variables to be pulled from the environment - Update Tautulli API dependency to 3.x

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
-FROM python:3
-
+FROM python:latest
 WORKDIR /app
-
 COPY requirements.txt ./
-
 RUN pip3 install -r requirements.txt
-
 COPY ./ /app
-
 CMD [ "python", "./Tauticord.py" ]
--- modules/config_parser.py
+++ modules/config_parser.py
@@ -1,63 +1,218 @@
+import os
+from typing import List
+
 import confuse
 
 
-class Config:
-    def __init__(self, app_name: str, config_path: str):
-        self.config = confuse.Configuration(app_name)
-        self.config.set_file(filename=config_path)
+def _extract_bool(value):
+    if isinstance(value, bool):
+        return value
+    if value.lower() in ["true", "yes", "1", "t"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "f"]:
+        return False
+    else:
+        raise ValueError("Not a boolean: {}".format(value))
+
+
+class ConfigSection:
+    def __init__(self, section_key: str, data, parent_key: str = None, pull_from_env: bool = True):
+        self.section_key = section_key
+        self.data = data
+        self.pull_from_env = pull_from_env
+        try:
+            self.data = data[self.section_key]
+        except confuse.NotFoundError:
+            pass
+        self._parent_key = parent_key
 
     @property
-    def _tautulli_config(self):
-        return self.config['Tautulli']
+    def full_key(self):
+        if self._parent_key is None:
+            return self.section_key
+        return f"{self._parent_key}_{self.section_key}".upper()
+
+    def _get_value(self, key: str, default=None, env_name_override: str = None):
+        if self.pull_from_env:
+            env_name = env_name_override or self.full_key
+            return os.getenv(env_name, default)
+        try:
+            return self.data[key].get()
+        except confuse.NotFoundError:
+            return default
+
+    def _get_subsection(self, key: str, default=None):
+        try:
+            return ConfigSection(section_key=key, parent_key=self.full_key, data=self.data,
+                                 pull_from_env=self.pull_from_env)
+        except confuse.NotFoundError:
+            return default
+
+
+class TautulliConfig(ConfigSection):
+    def __init__(self, data, pull_from_env: bool = True):
+        super().__init__(section_key="Tautulli", data=data, pull_from_env=pull_from_env)
 
     @property
-    def tautulli_connection_details(self):
-        return self._tautulli_config['Connection'].get()
+    def _connection(self) -> ConfigSection:
+        return self._get_subsection(key="Connection")
 
     @property
-    def tautulli_customization_details(self):
-        return self._tautulli_config['Customization'].get()
+    def api_key(self) -> str:
+        return self._connection._get_value(key="APIKey", env_name_override="TC_TAUTULLI_KEY")
 
     @property
-    def tautulli_voice_channels(self):
-        return self._tautulli_config['Customization']['VoiceChannels'].get()
+    def url(self) -> str:
+        return self._connection._get_value(key="URL", env_name_override="TC_TAUTULLI_URL")
 
     @property
-    def time_settings(self):
-        timezone = self.tautulli_customization_details.get('ServerTimeZone', None)
-        mil_time = self.tautulli_customization_details.get('Use24HourTime', True)
+    def _customization(self) -> ConfigSection:
+        return self._get_subsection(key="Customization")
+
+    @property
+    def has_plex_pass(self) -> bool:
+        value = self._customization._get_value(key="PlexPass", env_name_override="TC_PLEX_PASS",
+                                               default=False)
+        return _extract_bool(value)
+
+    @property
+    def refresh_interval(self) -> int:
+        return self._customization._get_value(key='RefreshSeconds', default=15,
+                                              env_name_override="TC_REFRESH_SECONDS")
+
+    @property
+    def terminate_message(self) -> str:
+        return self._customization._get_value(key='TerminateMessage', default="Your stream has ended.",
+                                              env_name_override="TC_TERMINATE_MESSAGE")
+
+    @property
+    def time_settings(self) -> dict:
+        timezone = self._customization._get_value(key='ServerTimeZone', default=None, env_name_override="TZ")
+        mil_time = self._customization._get_value(key='Use24HourTime', default=False,
+                                                  env_name_override="TC_USE_24_HOUR_TIME")
         return {'timezone': timezone,
                 'mil_time': mil_time}
 
     @property
-    def voice_channels(self):
-        _settings = self.tautulli_voice_channels
+    def _voice_channels(self) -> ConfigSection:
+        return self._customization._get_subsection(key="VoiceChannels")
+
+    @property
+    def display_stream_count(self) -> bool:
+        value = self._voice_channels._get_value(key="StreamCount", default=False,
+                                                env_name_override="TC_VC_STREAM_COUNT")
+        return _extract_bool(value)
+
+    @property
+    def display_transcode_count(self) -> bool:
+        value = self._voice_channels._get_value(key="TranscodeCount", default=False,
+                                                env_name_override="TC_VC_TRANSCODE_COUNT")
+        return _extract_bool(value)
+
+    @property
+    def display_bandwidth(self) -> bool:
+        value = self._voice_channels._get_value(key="Bandwidth", default=False,
+                                                env_name_override="TC_VC_BANDWIDTH")
+        return _extract_bool(value)
+
+    @property
+    def display_local_bandwidth(self) -> bool:
+        value = self._voice_channels._get_value(key="LocalBandwidth", default=False,
+                                                env_name_override="TC_VC_LOCAL_BANDWIDTH")
+        return _extract_bool(value)
+
+    @property
+    def display_remote_bandwidth(self) -> bool:
+        value = self._voice_channels._get_value(key="RemoteBandwidth", default=False,
+                                                env_name_override="TC_VC_REMOTE_BANDWIDTH")
+        return _extract_bool(value)
+
+    @property
+    def display_library_stats(self) -> bool:
+        value = self._voice_channels._get_value(key="LibraryStats", default=False,
+                                                env_name_override="TC_VC_LIBRARY_STATS")
+        return _extract_bool(value)
+
+    @property
+    def library_names(self) -> List[str]:
+        names = self._voice_channels._get_value(key="LibraryNames", default=[], env_name_override="TC_VC_LIBRARY_NAMES")
+        if isinstance(names, str):
+            return names.split(",")
+        return names
+
+    @property
+    def voice_channel_settings(self):
         return {
-            'count': _settings.get('StreamCount', False),
-            'transcodes': _settings.get('TranscodeCount', False),
-            'bandwidth': _settings.get('Bandwidth', False),
-            'localBandwidth': _settings.get('LocalBandwidth', False),
-            'remoteBandwidth': _settings.get('RemoteBandwidth', False),
-            'stats': _settings.get('LibraryStats', False),
-            'libraries': _settings.get('LibraryNames', [])
+            'count': self.display_stream_count,
+            'transcodes': self.display_transcode_count,
+            'bandwidth': self.display_bandwidth,
+            'localBandwidth': self.display_local_bandwidth,
+            'remoteBandwidth': self.display_remote_bandwidth,
+            'stats': self.display_library_stats,
+            'libraries': self.library_names
         }
 
-    @property
-    def _discord_config(self):
-        return self.config['Discord']
+
+class DiscordConfig(ConfigSection):
+    def __init__(self, data, pull_from_env: bool = True):
+        super().__init__(section_key="Discord", data=data, pull_from_env=pull_from_env)
 
     @property
-    def discord_connection_details(self):
-        return self._discord_config['Connection'].get()
+    def _connection(self) -> ConfigSection:
+        return self._get_subsection(key="Connection")
 
     @property
-    def discord_customization_details(self):
-        return self._discord_config['Customization'].get()
+    def bot_token(self) -> str:
+        return self._connection._get_value(key="BotToken", env_name_override="TC_DISCORD_BOT_TOKEN")
 
     @property
-    def allow_analytics(self):
-        return self.config['Extras']['Analytics'].get(bool) == True
+    def server_id(self) -> str:
+        return self._connection._get_value(key="ServerID", env_name_override="TC_DISCORD_SERVER_ID")
 
     @property
-    def log_level(self):
-        return self.config['logLevel'].get()
+    def owner_id(self) -> str:
+        return self._connection._get_value(key="OwnerID", env_name_override="TC_DISCORD_OWNER_ID")
+
+    @property
+    def channel_name(self) -> str:
+        return self._connection._get_value(key="ChannelName", env_name_override="TC_DISCORD_CHANNEL_NAME")
+
+    @property
+    def _customization(self) -> ConfigSection:
+        return self._get_subsection(key="Customization")
+
+    @property
+    def use_embeds(self) -> bool:
+        value = self._get_value(key="UseEmbeds", default=False, env_name_override="TC_USE_EMBEDS")
+        return _extract_bool(value)
+
+
+class ExtrasConfig(ConfigSection):
+    def __init__(self, data, pull_from_env: bool = True):
+        super().__init__(section_key="Extras", data=data, pull_from_env=pull_from_env)
+
+    @property
+    def allow_analytics(self) -> bool:
+        value = self._get_value(key="Analytics", default=True,
+                                env_name_override="TC_ALLOW_ANALYTICS")
+        return _extract_bool(value)
+
+
+class Config:
+    def __init__(self, app_name: str, config_path: str, fallback_to_env: bool = True):
+        self.config = confuse.Configuration(app_name)
+        self.pull_from_env = False
+        try:
+            self.config.set_file(filename=config_path)
+        except FileNotFoundError:
+            if not fallback_to_env:
+                raise FileNotFoundError(f"Config file not found: {config_path}")
+            self.pull_from_env = True
+
+        self.tautulli = TautulliConfig(self.config, self.pull_from_env)
+        self.discord = DiscordConfig(self.config, self.pull_from_env)
+        self.extras = ExtrasConfig(self.config, self.pull_from_env)
+        try:
+            self.log_level = self.config['logLevel'].get() or "INFO"
+        except confuse.NotFoundError:
+            self.log_level = "WARN"
