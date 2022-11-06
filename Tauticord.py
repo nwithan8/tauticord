4ff69d1d888186a0b560cfecf80f96a44790ba8b
nharris <nharris@easypost.com>
2022-11-06 16:59:48 -0700

    - linting

--- Tauticord.py
+++ Tauticord.py
@@ -4,11 +4,11 @@
 # Please see the LICENSE file that should have been included as part of this package.
 from pathlib import Path
 
-from modules.analytics import GoogleAnalytics
 import modules.discord_connector as discord
+import modules.logs as logging
 import modules.tautulli_connector as tautulli
 from modules import config_parser
-import modules.logs as logging
+from modules.analytics import GoogleAnalytics
 
 config = config_parser.Config(app_name="Tauticord", config_path=f"{Path(Path(__file__).parent / 'config.yaml')}")
 
@@ -20,23 +20,26 @@ logging.init(app_name="Tauticord", console_log_level=config.log_level, log_to_fi
 
 if __name__ == '__main__':
     logging.info("Starting application...")
-    t = tautulli.TautulliConnector(base_url=config.tautulli.url,
-                                   api_key=config.tautulli.api_key,
-                                   terminate_message=config.tautulli.terminate_message,
-                                   analytics=analytics,
-                                   use_embeds=config.discord.use_embeds,
-                                   plex_pass=config.tautulli.has_plex_pass,
-                                   voice_channel_settings=config.tautulli.voice_channel_settings,
-                                   time_settings=config.tautulli.time_settings
-                                   )
-    d = discord.DiscordConnector(token=config.discord.bot_token,
-                                 guild_id=config.discord.server_id,
-                                 admin_ids=config.discord.admin_ids,
-                                 refresh_time=config.tautulli.refresh_interval,
-                                 library_refresh_time=config.tautulli.library_refresh_interval,
-                                 tautulli_channel_name=config.discord.channel_name,
-                                 tautulli_connector=t,
-                                 analytics=analytics,
-                                 use_embeds=config.discord.use_embeds,
-                                 )
+
+    d = discord.DiscordConnector(
+        token=config.discord.bot_token,
+        guild_id=config.discord.server_id,
+        admin_ids=config.discord.admin_ids,
+        refresh_time=config.tautulli.refresh_interval,
+        library_refresh_time=config.tautulli.library_refresh_interval,
+        tautulli_channel_name=config.discord.channel_name,
+        tautulli_connector=tautulli.TautulliConnector(
+            base_url=config.tautulli.url,
+            api_key=config.tautulli.api_key,
+            terminate_message=config.tautulli.terminate_message,
+            analytics=analytics,
+            use_embeds=config.discord.use_embeds,
+            plex_pass=config.tautulli.has_plex_pass,
+            voice_channel_settings=config.tautulli.voice_channel_settings,
+            time_settings=config.tautulli.time_settings
+        ),
+        analytics=analytics,
+        use_embeds=config.discord.use_embeds,
+    )
+
     d.connect()
